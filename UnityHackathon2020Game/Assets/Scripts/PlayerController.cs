a03064d11294274d59c9a7d8f848af4011e1932a
Munch42 <munch4242@gmail.com>
2020-10-10 14:56:51 -0600

    Added Player Movement and Running

---
+++ UnityHackathon2020Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Character Attributes:")]
+    public float baseMovementSpeed = 2.0f;
+    public float baseRunningSpeedMultiplier = 2.0f;
+
+    [Space]
+    [Header("Character Statistics:")]
+    public Vector2 movementDirection;
+    public float movementSpeed;
+    public bool isRunning = false;
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+
+        if (Input.GetKey("left shift") || Input.GetKey("right shift"))
+        {
+            isRunning = true;
+        } else
+        {
+            isRunning = false;
+        }
+    }
+
+    void Move()
+    {
+        if (isRunning)
+        {
+            rb.velocity = movementDirection * movementSpeed * baseMovementSpeed * baseRunningSpeedMultiplier;
+        } else
+        {
+            rb.velocity = movementDirection * movementSpeed * baseMovementSpeed;
+        }
+    }
+}
