22ba61d46764f0c71a79a4a118ef932783326ace
masonb21 <72669920+masonb21@users.noreply.github.com>
2020-10-11 05:26:16 -0600

    Intro text and improvements to UI

---
+++ UnityHackathon2020Game/Assets/Scripts/IntroText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class IntroText : MonoBehaviour
+{
+    float time = 7.0f;
+    private Text txt;
+
+    private void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+            return;
+        }
+        else
+        {
+            txt.enabled = false;
+        }
+    }
+}
