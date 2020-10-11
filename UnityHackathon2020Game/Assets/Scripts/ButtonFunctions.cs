97c8b9a646d8a4ed5a2dd5b3f92638343f2b22f3
Monkey042 <Monkey042042@gmail.com>
2020-10-11 14:54:51 -0600

    Added Main Menu

---
+++ UnityHackathon2020Game/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public Button playButton;
+    public GameObject levelButtons;
+
+    public void Back(){
+        playButton.gameObject.SetActive(true);
+        levelButtons.SetActive(false);
+    }
+    public void SelectLevel(){
+        playButton.gameObject.SetActive(false);
+        levelButtons.SetActive(true);
+    }
+    public void Play(int sceneIndex){
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
