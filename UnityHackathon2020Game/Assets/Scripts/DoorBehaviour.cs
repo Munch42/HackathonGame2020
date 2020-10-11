c807072bc45ff03c751a6a0c3f69f67ecf221db4
masonb21 <72669920+masonb21@users.noreply.github.com>
2020-10-10 19:17:38 -0600

    Added switch and door functionality
    switch and door functionality with place holder door and lever sprites

---
+++ UnityHackathon2020Game/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private bool switched = false;
+
+    //Get reference to switch that will open the door
+    public Switch doorSwitch;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //see if switch has been switched
+        switched = doorSwitch.isSwitched();
+
+        //if door is open, hide from veiw
+        //else keep door closed
+        if (switched)
+        {
+            sr.enabled = false;
+        }
+        else
+        {
+            sr.enabled = true;
+        }
+    }
+}
---
+++ UnityHackathon2020Game/Assets/Scripts/Switch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    private bool switched = false;
+
+    private void OnMouseOver()
+    { 
+        //if object is clicked, change rotation and value of switched
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (switched)
+            {
+                switched = false;
+                gameObject.transform.eulerAngles = new Vector3(0,0,0);
+            }
+            else
+            {
+                switched = true;
+                gameObject.transform.eulerAngles = new Vector3(0, 0, -65);
+
+            }
+        }
+    }
+
+    //public reference to switched
+    public bool isSwitched()
+    {
+        return switched;
+    }
+}
