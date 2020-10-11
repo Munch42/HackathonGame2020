74095784f027ac791f4488246b7260fd723a2bb7
masonb21 <72669920+masonb21@users.noreply.github.com>
2020-10-11 01:38:48 -0600

    Added pedistol challenge
    player can now push barrel onto pedistol to disable spikes

---
+++ UnityHackathon2020Game/Assets/Scripts/Pedistol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedistol : MonoBehaviour
+{
+    private bool isPressed = false;
+
+    public GameObject barrel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.Equals(barrel.GetComponent<BoxCollider2D>()))
+        {
+            isPressed = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.Equals(barrel.GetComponent<BoxCollider2D>()))
+        {
+            isPressed = false;
+        }
+    }
+
+    public bool IsPressed()
+    {
+        return isPressed;
+    }
+}
---
+++ UnityHackathon2020Game/Assets/Scripts/PedistolSpikes.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedistolSpikes : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Sprite up;
+    private bool switched = false;
+    private BoxCollider2D bCollider;
+
+    public Sprite down;
+
+    //Get reference to switch that will open the door
+    public Pedistol pedistol;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        up = sr.sprite;
+        bCollider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //see if switch has been switched
+        switched = pedistol.IsPressed();
+
+        //if door is open, hide from veiw
+        //else keep door closed
+        if (switched)
+        {
+            sr.sprite = down;
+            bCollider.enabled = false;
+        }
+        else
+        {
+            sr.sprite = up;
+            bCollider.enabled = true;
+        }
+    }
+}
